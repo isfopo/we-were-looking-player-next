c5af6eae7f58f5b23ffe0b869134e35f6636d3c8
Isaac Poole <isaacpoolemusic@gmail.com>
2021-01-20 22:38:58 -0500

    switch to vimeo

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 
-export const  VideoPlayer = (props) => {
-    const { source, isPlaying, setIsLoaded, next } = props;
+export const  VideoPlayer = ({ source, isPlaying, setIsLoaded, next, vimeoCode }) => {
 
     return (
         <div className="player">
             <ReactPlayer 
-                url={`./video/${source}.mp4`} 
+                url={`https://vimeo.com/${vimeoCode}`} 
+                config={{
+                    vimeo: {
+                        playerOptions: { controls: false }
+                    },
+                  }}
                 playing={isPlaying}
                 onReady={setIsLoaded}
                 onEnded={() => next()}
-                width='100%'
-                height='100%'
             />
         </div>
     );

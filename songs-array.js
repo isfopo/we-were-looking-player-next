67f79b1fde191ab41962d3536c20acee33d50e53
Isaac Poole <isaacpoolemusic@gmail.com>
2021-01-20 22:38:31 -0500

    adds vimdeo codes

--- songs-array.js
+++ songs-array.js
@@ -1,83 +1,12 @@
 export const songsArray = [
     {
-        name : "immortal in his own sense",
-        album: "we-were-looking",
-        fileName : "immortal-in-his-own-sense",
-        backgroundColor : "#CACCFA",
-        releaseDate : "February 24, 2021 00:00:00",
-        iconColor : "dark"
-    },
-    {
-        name : "he steps into a dark room",
-        album: "we-were-looking",
-        fileName : "he-steps-into-a-dark-room",
-        backgroundColor : "#B8CBEA",
-        releaseDate : "March 10, 2021 00:00:00",
-        iconColor : "dark"
-    },
-    {
-        name : "small, unnoticeable events",
-        album: "we-were-looking",
-        fileName : "small-unnoticeable-events",
-        backgroundColor : "#CAD3FA",
-        releaseDate : "March 24, 2021 00:00:00",
-        iconColor : "dark"
-    },
-    {
-        name : "pressed up against a wall of teenage angst",
-        album: "we-were-looking",
-        fileName : "pressed-up-against-a-wall-of-teenage-angst",
-        backgroundColor : "#B4D9EB",
-        releaseDate : "April 7, 2021 00:00:00",
-        iconColor : "dark"
-    },
-    {
-        name : "here is the world there is beyond",
-        album: "we-were-looking",
-        fileName : "here-is-the-world-there-is-beyond",
-        backgroundColor : "#1C3954",
-        releaseDate : "April 21, 2021 00:00:00",
-        iconColor : "light"
-    },
-    {
-        name : "kept on pushing and pushing and pushing",
-        album: "we-were-looking",
-        fileName : "kept-on-pushing-and-pushing-and-pushing",
-        backgroundColor : "#023B58",
-        releaseDate : "May 5, 2021 00:00:00",
-        iconColor : "light"
-    },
-    {
-        name : "as if it were some purified existence",
-        album: "we-were-looking",
-        fileName : "as-if-it-were-some-purified-existence",
-        backgroundColor : "#182354",
-        releaseDate : "May 19, 2021 00:00:00",
-        iconColor : "light"
-    },
-    {
-        name : "no tricks, not yet",
-        album: "we-were-looking",
-        fileName : "no-tricks-not-yet",
-        backgroundColor : "#161730",
-        releaseDate : "June 2, 2021 00:00:00",
-        iconColor : "light"
-    },
-    {
-        name : "angel, cut and cold",
-        album: "we-were-looking",
-        fileName : "angel-cut-and-cold",
-        backgroundColor : "#17253D",
-        releaseDate : "June 16, 2021 00:00:00",
-        iconColor : "light"
-    },
-    {
         name : "Enter City Limits",
         album: "the-solo-ep",
         fileName : "enter-city-limits",
         backgroundColor : "#ae1667",
         releaseDate : "January 15, 2021 00:00:00",
-        iconColor : "dark"
+        iconColor : "dark",
+        vimeoCode: "501270551",
     },
     {
         name : "A Brief History",
@@ -85,7 +14,8 @@ export const songsArray = [
         fileName : "a-brief-history",
         backgroundColor : "#ae1667",
         releaseDate : "January 15, 2021 00:00:00",
-        iconColor : "dark"
+        iconColor : "dark",
+        vimeoCode: "502747684",
     },
     {
         name : "And the Spirit Changes",
@@ -93,15 +23,97 @@ export const songsArray = [
         fileName : "and-the-spirit-changes",
         backgroundColor : "#ae1667",
         releaseDate : "January 15, 2021 00:00:00",
-        iconColor : "dark"
+        iconColor : "dark",
+        vimeoCode: "502748255",
     },
-
     {
         name : "War and Peace in the Land of Machines",
         album: "the-solo-ep",
         fileName : "war-and-peace-in-the-land-of-machines",
         backgroundColor : "#ae1667",
         releaseDate : "January 15, 2021 00:00:00",
-        iconColor : "dark"
+        iconColor : "dark",
+        vimeoCode: "501270704",
+    },
+    {
+        name : "immortal in his own sense",
+        album: "we-were-looking",
+        fileName : "immortal-in-his-own-sense",
+        backgroundColor : "#CACCFA",
+        releaseDate : "February 24, 2021 00:00:00",
+        iconColor : "dark",
+        vimeoCode: "",
+    },
+    {
+        name : "he steps into a dark room",
+        album: "we-were-looking",
+        fileName : "he-steps-into-a-dark-room",
+        backgroundColor : "#B8CBEA",
+        releaseDate : "March 10, 2021 00:00:00",
+        iconColor : "dark",
+        vimeoCode: "501271152",
+    },
+    {
+        name : "small, unnoticeable events",
+        album: "we-were-looking",
+        fileName : "small-unnoticeable-events",
+        backgroundColor : "#CAD3FA",
+        releaseDate : "March 24, 2021 00:00:00",
+        iconColor : "dark",
+        vimeoCode: "",
+    },
+    {
+        name : "pressed up against a wall of teenage angst",
+        album: "we-were-looking",
+        fileName : "pressed-up-against-a-wall-of-teenage-angst",
+        backgroundColor : "#B4D9EB",
+        releaseDate : "April 7, 2021 00:00:00",
+        iconColor : "dark",
+        vimeoCode: "",
+    },
+    {
+        name : "here is the world there is beyond",
+        album: "we-were-looking",
+        fileName : "here-is-the-world-there-is-beyond",
+        backgroundColor : "#1C3954",
+        releaseDate : "April 21, 2021 00:00:00",
+        iconColor : "light",
+        vimeoCode: "",
+    },
+    {
+        name : "kept on pushing and pushing and pushing",
+        album: "we-were-looking",
+        fileName : "kept-on-pushing-and-pushing-and-pushing",
+        backgroundColor : "#023B58",
+        releaseDate : "May 5, 2021 00:00:00",
+        iconColor : "light",
+        vimeoCode: "501271101",
+    },
+    {
+        name : "as if it were some purified existence",
+        album: "we-were-looking",
+        fileName : "as-if-it-were-some-purified-existence",
+        backgroundColor : "#182354",
+        releaseDate : "May 19, 2021 00:00:00",
+        iconColor : "light",
+        vimeoCode: "",
+    },
+    {
+        name : "no tricks, not yet",
+        album: "we-were-looking",
+        fileName : "no-tricks-not-yet",
+        backgroundColor : "#161730",
+        releaseDate : "June 2, 2021 00:00:00",
+        iconColor : "light",
+        vimeoCode: "",
+    },
+    {
+        name : "angel, cut and cold",
+        album: "we-were-looking",
+        fileName : "angel-cut-and-cold",
+        backgroundColor : "#17253D",
+        releaseDate : "June 16, 2021 00:00:00",
+        iconColor : "light",
+        vimeoCode: "",
     },
 ]
